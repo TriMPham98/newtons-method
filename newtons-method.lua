52e46d65c7fefcfd12c798dd3f6391f6396d2e36
Tri <trimpham98@gmail.com>
2023-10-02 11:58:04 -0700

    Add max tolerance

--- newtons-method.lua
+++ newtons-method.lua
@@ -1,15 +1,18 @@
 function iterFunc(x)
-    return x - (x^3 + x - 1) / (3*x^2 + 1)
+    return x - (-x^4 + 3*x^2 + 2) / (-4*x^3+6*x)
 end
 
-oldEst = 0
-tol = 0.00005
+oldEst = 1
+tol = 0.000000005
+n = 0
+nmax = 50
 
 error = 1
 
-while (error > tol) do
+while (error > tol and n < nmax) do
     newEst = iterFunc(oldEst)
     print(newEst)
     error = math.abs(newEst - oldEst)
     oldEst = newEst
+    n = n + 1
 end
